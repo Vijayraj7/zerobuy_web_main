6b9e625f3d48dd1118cd19b1b513cecedfd98285
VIJAYRAJ RAHUL E B <vijayrajrahul515@gmail.com>
2025-11-27 15:51:51 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -178,6 +178,7 @@ Route::middleware(['auth:sanctum', 'role:customer'])->group(function () {
 
     Route::controller(ReturnOrderController::class)->group(function () {
         Route::get('/return-orders', 'index');
+        Route::get('/return-order-details/{returnOrder}', 'show');
         Route::post('/return-order', 'store');
     });
 
