d31bb1264a14b58b481fcc5c81b9f32835e74e70
VIJAYRAJ RAHUL E B <vijayrajrahul515@gmail.com>
2025-12-04 15:46:02 +0530

    Update SellerProductDetailsResource.php

--- app/Http/Resources/SellerProductDetailsResource.php
+++ app/Http/Resources/SellerProductDetailsResource.php
@@ -19,6 +19,7 @@ class SellerProductDetailsResource extends JsonResource
             'name' => $this->name,
             'price' => (float) $this->price,
             'discount_price' => (float) $this->discount_price,
+            'details' =>  $this->details,
             'quantity' => (int) $this->quantity,
             'code' => (int) $this->code,
             'min_order_quantity' => (int) $this->min_order_quantity,
