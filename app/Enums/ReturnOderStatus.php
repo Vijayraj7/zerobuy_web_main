983b133c2676d9a9722cd8a3ac8810439ec2d7f5
VIJAYRAJ RAHUL E B <vijayrajrahul515@gmail.com>
2025-12-09 17:57:44 +0530

    status

--- app/Enums/ReturnOderStatus.php
+++ app/Enums/ReturnOderStatus.php
@@ -7,7 +7,9 @@ enum ReturnOderStatus: string
     case PENDING = 'Pending';
     case APPROVED = 'Approved';
     case COMPLETED = 'Completed';
+    case REJECTED = 'Rejected';
+    case CANCELLED = 'Cancelled';
+
     case DAMAGED = 'Damaged';
     case MISMATCH = 'Mismatch';
-    case REJECTED = 'Rejected';
 }
--- routes/seller.php
+++ routes/seller.php
@@ -67,7 +67,7 @@ Route::prefix('/seller')->group(function () {
         Route::controller(ReturnOrderController::class)->group(function () {
             Route::get('/return-orders', 'index');
             Route::get('/return/{returnOrder}/show', 'show');
-            Route::post('/return/{returnOrder}/update-status', 'statusChange');
+            Route::get('/return/{returnOrder}/update-status', 'statusChange');
         });
 
         // wallet route
