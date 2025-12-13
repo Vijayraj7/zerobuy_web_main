95b43def8bbd0997ab173c39ccd849f652529d4d
VIJAYRAJ RAHUL E B <vijayrajrahul515@gmail.com>
2025-12-13 13:36:28 +0530

    variant

--- app/Models/ProductVariant.php
+++ app/Models/ProductVariant.php
@@ -4,14 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ProductVariant extends Model
 {
     use HasFactory;
-    protected $fillable = ['product_id','color_id','size_id','price','quantity'];
+    protected $fillable = ['product_id', 'color_id', 'size_id', 'price', 'quantity'];
 
-    public function product() 
-    { 
-        return $this->belongsTo(Product::class); 
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function color(): BelongsTo
+    {
+        return $this->belongsTo(Color::class);
+    }
+
+    public function size(): BelongsTo
+    {
+        return $this->belongsTo(Size::class);
     }
 }
