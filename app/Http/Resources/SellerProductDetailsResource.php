522fad07d15abdfea899df725a95fc624d40c748
VIJAYRAJ RAHUL E B <vijayrajrahul515@gmail.com>
2025-12-13 13:38:42 +0530

    Update SellerProductDetailsResource.php

--- app/Http/Resources/SellerProductDetailsResource.php
+++ app/Http/Resources/SellerProductDetailsResource.php
@@ -24,7 +24,7 @@ class SellerProductDetailsResource extends JsonResource
             'code' => (int) $this->code,
             'min_order_quantity' => (int) $this->min_order_quantity,
             'thumbnail' => $this->thumbnail,
-            'variants' => $this->variants,
+            'variants' => ProductDetailsResource::collection($this->variants),
             // 'videourl' => $this->videourl(),
             'additional_thumbnail' => $this->additionalThumbnails(),
             'category' => OnlyCategoryResource::make($this->categories()?->first()),
