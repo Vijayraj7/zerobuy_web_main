0b5fc81866d0216a429e72f11b47436575ccbbb0
VIJAYRAJ RAHUL E B <vijayrajrahul515@gmail.com>
2025-12-15 13:08:57 +0530

    d

--- app/Http/Controllers/API/Seller/DeliverySettingController.php
+++ app/Http/Controllers/API/Seller/DeliverySettingController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\API;
+namespace App\Http\Controllers\API\Seller;
 
 use App\Http\Controllers\Controller;
 use App\Models\DeliverySetting;
--- routes/seller.php
+++ routes/seller.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\ChatController;
-use App\Http\Controllers\API\DeliverySettingController;
+use App\Http\Controllers\API\Seller\DeliverySettingController;
 use App\Http\Controllers\API\Seller\BannerController;
 use App\Http\Controllers\API\Seller\DashboardController;
 use App\Http\Controllers\API\Seller\LoginController;
