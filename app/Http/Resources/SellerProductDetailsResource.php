0f89ff91eae24043ecbd53893ad79807d22338e8
VIJAYRAJ RAHUL E B <vijayrajrahul515@gmail.com>
2025-12-17 15:57:27 +0530

    gst

--- app/Http/Resources/SellerProductDetailsResource.php
+++ app/Http/Resources/SellerProductDetailsResource.php
@@ -24,6 +24,7 @@ class SellerProductDetailsResource extends JsonResource
             'code' => (int) $this->code,
             'min_order_quantity' => (int) $this->min_order_quantity,
             'thumbnail' => $this->thumbnail,
+            'gst' => $this->tax_percentage,
             'condition_status' => $this->condition_status,
             'variants' => ProductVariantResource::collection($this->variants),
             // 'videourl' => $this->videourl(),
--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -254,6 +254,7 @@ class ProductRepository extends Repository
             'min_order_quantity' => $request->min_order_quantity ?? 1,
             'media_id' => $thumbnail ? $thumbnail->id : null,
             // 'code' => $request->code,
+            'tax_percentage' => $request->gst ?? 0,
             'condition_status' => $request->condition_status ?? 'New',
             'buy_price' => $request->buy_price ?? 0,
             'is_active' => $isAdmin ? true : $approve,
