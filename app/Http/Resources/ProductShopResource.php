a8936453160144f8c789dbd34d4d9c3d4da78bec
VIJAYRAJ RAHUL E B <vijayrajrahul515@gmail.com>
2025-12-22 17:52:17 +0530

    Update ProductShopResource.php

--- app/Http/Resources/ProductShopResource.php
+++ app/Http/Resources/ProductShopResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\ShopFollower;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -20,7 +21,21 @@ class ProductShopResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'logo' => $this->logo,
-            'rating' => (float) ($this->averageRating > 0) ? $this->averageRating : 5.0,
+            'banner' => $this->banner,
+            'state' => $this->state,
+            'district' => $this->district,
+            'is_followed' => (bool) $this->is_followed,
+            'followers' => (int) ShopFollower::where('shop_id', $this->id)->count(),
+            'total_products' => (int) $this->products()->isActive()->count(),
+            'total_categories' => (int) $this->categories()->active()->count(),
+            'rating' => (float) number_format($this->averageRating, 1, '.', ''),
+            'total_reviews' => (int) $this->reviews->count(),
+            // 'shop_status' => (string) $shopStatus,
+            'short_description' => $this->short_description,
+            'description' => $this->description,
+            'is_verified' => $this->is_verified == 1 ? true : false,
+            'is_branded' => $this->is_branded == 1 ? true : false,
+            'banners' => BannerResource::collection($this->banners()->where('status', 1)->get()),
             'estimated_delivery_time' => (string) ($this->estimated_delivery_time ?? '2-3 days'),
             'delivery_charge' => (float) getDeliveryCharge(1),
             'last_online' => $lastOnline
