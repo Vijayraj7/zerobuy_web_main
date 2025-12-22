d9d7b5cc3c540a1417d9bc15352dac76ac33c7f2
VIJAYRAJ RAHUL E B <vijayrajrahul515@gmail.com>
2025-12-22 21:48:51 +0530

    Update AjaxCategoryController.php

--- app/Http/Controllers/Admin/AjaxCategoryController.php
+++ app/Http/Controllers/Admin/AjaxCategoryController.php
@@ -12,25 +12,33 @@ class AjaxCategoryController extends Controller
 {
     public function categories($businessCategoryId)
     {
-        return Cache::remember(
-            "categories_$businessCategoryId",
-            3600,
-            fn () => Category::select('id','name')
-                ->where('business_category_id', $businessCategoryId)
-                ->where('status', 1)
-                ->get()
-        );
+        // return Cache::remember(
+        //     "categories_$businessCategoryId",
+        //     3600,
+        //     fn () => Category::select('id','name')
+        //         ->where('business_category_id', $businessCategoryId)
+        //         ->where('status', 1)
+        //         ->get()
+        // );
+        return  Category::select('id', 'name')
+            ->where('business_category_id', $businessCategoryId)
+            ->where('status', 1)
+            ->get();
     }
 
     public function subCategories($categoryId)
     {
-        return Cache::remember(
-            "subcategories_$categoryId",
-            3600,
-            fn () => SubCategory::select('id','name')
-                ->where('category_id', $categoryId)
-                ->where('is_active', 1)
-                ->get()
-        );
+        // return Cache::remember(
+        //     "subcategories_$categoryId",
+        //     3600,
+        //     fn () => SubCategory::select('id','name')
+        //         ->where('category_id', $categoryId)
+        //         ->where('is_active', 1)
+        //         ->get()
+        // );
+        return SubCategory::select('id', 'name')
+            ->where('category_id', $categoryId)
+            ->where('is_active', 1)
+            ->get();
     }
 }
