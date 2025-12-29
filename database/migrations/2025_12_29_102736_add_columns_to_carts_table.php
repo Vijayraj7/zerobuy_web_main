34a7b81b373cb8c3e5c6dd011858441de15e804c
VIJAYRAJ RAHUL E B <vijayrajrahul515@gmail.com>
2025-12-29 12:09:08 +0530

    Create 2025_12_29_102736_add_columns_to_carts_table.php

---
+++ database/migrations/2025_12_29_102736_add_columns_to_carts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('carts', function (Blueprint $table) {
+            //
+            $table->string('bulk_price_id')->nullable();
+            $table->string('variant_id')->nullable();
+            $table->longText('bulk_items')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('carts', function (Blueprint $table) {
+            //
+        });
+    }
+};
