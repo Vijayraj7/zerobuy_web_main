3c066ad36d6272c16dca811659ebf255ab1bdd95
VIJAYRAJ RAHUL E B <vijayrajrahul515@gmail.com>
2026-01-07 17:30:07 +0530

    Update OrderProductResource.php

--- app/Http/Resources/OrderProductResource.php
+++ app/Http/Resources/OrderProductResource.php
@@ -19,7 +19,7 @@ class OrderProductResource extends JsonResource
 
         $review = $this->reviews()->where('customer_id', auth()->user()->customer?->id)->where('product_id', $this->id)->where('order_id', $request->order_id)->first();
 
-        $price = $this->price > 0 ? $this->price : ($this->discount_price > 0 ? $this->discount_price : $this->price);
+        $price = $this->pivot->price > 0 ? $this->pivot->price : ($this->discount_price > 0 ? $this->discount_price : $this->price);
 
         $isReturned = ReturnOrderDetail::where('product_id', $this->id)
             ->whereHas('returnOrder', function ($q) use ($request) {
@@ -28,7 +28,7 @@ class OrderProductResource extends JsonResource
             ->exists();
         $isReturnable = ! $isReturned;
 
-        $orderCreatedAt = $this?->order?->created_at;
+        $orderCreatedAt = $this->pivot?->order?->created_at;
         $returnable = false;
 
         $returnPeriod = !empty($this->return_period)
@@ -53,16 +53,16 @@ class OrderProductResource extends JsonResource
         $pname = $this->name;
         $dprice = 0;
         $mprice = (float) number_format((float) $this->price, 2, '.', '');
-        $color = $this->color ?? null;
-        $size = $this->size ?? null;
-        if ($this->orderVariant) {
-            $dprice =  (float) number_format($this->orderVariant->price, 2, '.', '');
-            $color =  $this->orderVariant->color_name;
-            $size =  $this->orderVariant->size_name;
-        } else if ($this->orderBulkItem) {
-            $pname = $this->orderBulkItem->name;
-            $mprice = (float) number_format($this->orderBulkItem->mrp, 2, '.', '');
-            $dprice =  (float) number_format($this->orderBulkItem->selling_price, 2, '.', '');
+        $color = $this->pivot->color ?? null;
+        $size = $this->pivot->size ?? null;
+        if ($this->pivot->orderVariant) {
+            $dprice =  (float) number_format($this->pivot->orderVariant->price, 2, '.', '');
+            $color =  $this->pivot->orderVariant->color_name;
+            $size =  $this->pivot->orderVariant->size_name;
+        } else if ($this->pivot->orderBulkItem) {
+            $pname = $this->pivot->orderBulkItem->name;
+            $mprice = (float) number_format($this->pivot->orderBulkItem->mrp, 2, '.', '');
+            $dprice =  (float) number_format($this->pivot->orderBulkItem->selling_price, 2, '.', '');
         } else {
             $dprice = (float) number_format($this->discount_price, 2, '.', '');
         }
@@ -75,15 +75,15 @@ class OrderProductResource extends JsonResource
             'price' => (float) $this->price,
             'd_price' => (float) ($dprice > 0 ? $dprice : $mprice),
             'discount_price' => (float) ($dprice > 0 ? $dprice : 0),
-            'order_qty' => (int) $this->quantity,
+            'order_qty' => (int) $this->pivot->quantity,
             'color' => $color,
             'size' => $size,
             'rating' => $review ? (float) $review->rating : null,
-            'unit' => $this->unit ?? null,
+            'unit' => $this->pivot->unit ?? null,
             'is_returned' => $isReturnable,
             'return_period' => $returnPeriod,
             'last_return_date' => $lastreturndate,
-            'order_date' => $this->order?->created_at,
+            'order_date' => $this->pivot->order?->created_at,
             'is_returnable' => $returnable && $isReturnable,
         ];
     }
