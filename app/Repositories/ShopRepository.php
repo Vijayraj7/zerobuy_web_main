6c843eb7a76e36cc0861e01d96eb21368e5d01a8
VIJAYRAJ RAHUL E B <vijayrajrahul515@gmail.com>
2026-01-07 13:08:23 +0530

    Update ShopRepository.php

--- app/Repositories/ShopRepository.php
+++ app/Repositories/ShopRepository.php
@@ -130,48 +130,51 @@ class ShopRepository extends Repository
         $shop->businessCategories()->sync($request->bussiness_categories_id);
 
 
-        DB::transaction(function () use ($request, $shop) {
+        if ($request->has('delivery_mode')) {
 
-            $setting = DeliverySetting::updateOrCreate(
-                ['shop_id' => $shop->id],
-                [
-                    'delivery_mode' => $request->delivery_mode,
-                    'update_when_shipped' => $request->update_when_shipped ?? false,
-                ]
-            );
+            DB::transaction(function () use ($request, $shop) {
 
-            // Clean old data
-            DeliveryAmountRule::where('delivery_setting_id', $setting->id)->delete();
-            DeliveryStateCharge::where('delivery_setting_id', $setting->id)->delete();
+                $setting = DeliverySetting::updateOrCreate(
+                    ['shop_id' => $shop->id],
+                    [
+                        'delivery_mode' => $request->delivery_mode,
+                        'update_when_shipped' => $request->update_when_shipped ?? false,
+                    ]
+                );
 
-            // Amount based
-            if ($request->delivery_mode === 'amount_based') {
-                foreach ($request->amount_rules ?? [] as $rule) {
-                    DeliveryAmountRule::create([
-                        'delivery_setting_id' => $setting->id,
-                        'min_amount' => $rule['min_amount'],
-                        'max_amount' => $rule['max_amount'],
-                        'charge'     => $rule['charge'],
-                    ]);
-                }
-            }
+                // Clean old data
+                DeliveryAmountRule::where('delivery_setting_id', $setting->id)->delete();
+                DeliveryStateCharge::where('delivery_setting_id', $setting->id)->delete();
 
-            // State wise
-            if ($request->delivery_mode === 'state_wise') {
-                foreach ($request->state_charges ?? [] as $state) {
-                    if ($state['charge'] != null) {
-                        $stateModel = State::find($state['state']);
-                        // dd($stateModel);
-                        DeliveryStateCharge::create([
+                // Amount based
+                if ($request->delivery_mode === 'amount_based') {
+                    foreach ($request->amount_rules ?? [] as $rule) {
+                        DeliveryAmountRule::create([
                             'delivery_setting_id' => $setting->id,
-                            'state' => $stateModel->name,
-                            'state_id' => $stateModel->id,
-                            'charge' => $state['charge'],
+                            'min_amount' => $rule['min_amount'],
+                            'max_amount' => $rule['max_amount'],
+                            'charge'     => $rule['charge'],
                         ]);
                     }
                 }
-            }
-        });
+
+                // State wise
+                if ($request->delivery_mode === 'state_wise') {
+                    foreach ($request->state_charges ?? [] as $state) {
+                        if ($state['charge'] != null) {
+                            $stateModel = State::find($state['state']);
+                            // dd($stateModel);
+                            DeliveryStateCharge::create([
+                                'delivery_setting_id' => $setting->id,
+                                'state' => $stateModel->name,
+                                'state_id' => $stateModel->id,
+                                'charge' => $state['charge'],
+                            ]);
+                        }
+                    }
+                }
+            });
+        }
 
 
         $stateIds = array_map('intval', $request->delivery_state_ids);
