86ad74fa64f36067efbad54af8c5ef76ea8f3c75
VIJAYRAJ RAHUL E B <vijayrajrahul515@gmail.com>
2026-01-07 16:53:56 +0530

    Update OrderController.php

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -66,7 +66,7 @@ class OrderController extends Controller
                 'delivered' => $statusWiseOrders[OrderStatus::DELIVERED->value],
                 'cancelled' => $statusWiseOrders[OrderStatus::CANCELLED->value],
             ],
-            'orders' => OrderResource::collection($orders),
+            'orders' => OrderResource::collection($customer->orders()),
         ]);
     }
 
