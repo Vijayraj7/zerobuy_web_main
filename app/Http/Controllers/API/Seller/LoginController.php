1e0a0eb5eeaea0eaa8bc8ab5da326b41c3f30264
VIJAYRAJ RAHUL E B <vijayrajrahul515@gmail.com>
2026-01-28 14:23:59 +0530

    Update LoginController.php

--- app/Http/Controllers/API/Seller/LoginController.php
+++ app/Http/Controllers/API/Seller/LoginController.php
@@ -101,7 +101,14 @@ class LoginController extends Controller
      */
     private function authenticate($request)
     {
-        $user = UserRepository::findByContact($request->contact);
+        $user = UserRepository::query()
+            ->where(function ($query) use ($request) {
+                $query->where('phone', $request->contact)
+                    ->orWhere('email', $request->contact);
+            })
+            ->whereHas('shop')
+            ->first();
+
         if (! is_null($user) && $user->shop) {
             if (Hash::check($request->password, $user->password)) {
                 return $user;
