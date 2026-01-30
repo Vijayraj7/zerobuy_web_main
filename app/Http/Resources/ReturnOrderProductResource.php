7d0ebc9ddafd90405a4a10c9fd996a5e5a2593b7
VIJAYRAJ RAHUL E B <vijayrajrahul515@gmail.com>
2026-01-30 12:33:06 +0530

    o

--- app/Http/Resources/ReturnOrderProductResource.php
+++ app/Http/Resources/ReturnOrderProductResource.php
@@ -16,12 +16,12 @@ class ReturnOrderProductResource extends JsonResource
     {
         return [
             'product_id' => (int)$this->product_id,
-            'product_name' => $this->product->name ?? '',
+            'product_name' => $this->product_name ?? '',
             'product_price' => (float)$this->price ?? '',
             'thumbnail' => $this->product->thumbnail ?? '',
             'quantity' => (int)$this->quantity,
-            'color' => $this->color,
-            'size' => $this->size,
+            'color' => $this->orderVariant?->color_name ?? $this->color,
+            'size' => $this->orderVariant?->size_name ?? $this->size,
             'unit' => $this->unit,
             'price' => (float)$this->price,
         ];
